14adf3bb5dd8a0de39a69e561eb8982e527977d2
ElmiraMatyeva <elmira.matyeva@gmail.com>
2017-03-31 16:44:22 +0600

    added controllers for Dishes and Restaurants

---
+++ app/controllers/dishes_controller.rb
@@ -0,0 +1,7 @@
+class DishesController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  get 'dishes/show'
+
+  get 'dishes/index'
+
+  get 'restaurants/show'
+
+  get 'restaurants/index'
+
+  root 'restaurants#index'
+  devise_for :users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
